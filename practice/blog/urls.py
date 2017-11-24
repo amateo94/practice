a04684dc30d028f7de38adec267462b3a6e2beb7
amateo94 <mateo.b.alec@gmail.com>
2017-11-24 17:15:15 -0500

    Added index page

--- practice/blog/urls.py
+++ practice/blog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^$', views.index, name='home'),
+    url(r'^index/(?P<pageNumber>[0-9]+)/$', views.index, name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     ]
--- practice/blog/views.py
+++ practice/blog/views.py
@@ -3,6 +3,17 @@ from .models import Post
 
 # Create your views here.
 
+def index(request, pageNumber = 0):
+    pageNumber = int(pageNumber)
+    startIndex = pageNumber * 10 + 1
+    endIndex = (pageNumber + 1) * 10
+    post_list = Post.objects.filter(id__range=(startIndex, endIndex))
+    return render(request, 'blog/index.html', context = {'posts': post_list, 
+                                                         'currentPage': pageNumber,
+                                                         'prevPage': pageNumber - 1,
+                                                         'nextPage': pageNumber + 1})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', context = {'post': post})
